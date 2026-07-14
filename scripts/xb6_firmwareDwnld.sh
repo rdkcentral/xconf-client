3139929331ba9bfabef4e79c60e361e30a84dc63
MonekaLakshmi <101797473+MonekaLakshmi@users.noreply.github.com>
2026-07-14 14:33:06 +0530

    Update xb6_firmwareDwnld.sh

--- scripts/xb6_firmwareDwnld.sh
+++ scripts/xb6_firmwareDwnld.sh
@@ -1324,16 +1324,13 @@ if [ "$type" != "PROD" ] && [ "$type" != "prod" ]; then
     else
         # RFC override should work only for non-production build
         url_override=`syscfg get AutoExcludedURL`
-        if [ "$url_override" ] ; then
+        # Set CDL_SERVER_OVERRIDE only for non-direct-CDN flow
+        if [ "$url_override" ] && [ "$direct_CDN" = "false" ]; then
            url=$url_override
            xconf_url=$url
-           # Set CDL_SERVER_OVERRIDE only for non-direct-CDN flow
-	   if [ "$direct_CDN" = "false" ]; then
 	       CDL_SERVER_OVERRIDE=1
-	       echo "XCONF SCRIPT : Direct CDN NOT ENABLED. CDL_SERVER_OVERRIDE is set URL=$url"  >> $XCONF_LOG_FILE
-	   else
-	       echo "XCONF SCRIPT : Direct CDN enabled. Not setting CDL_SERVER_OVERRIDE for AutoExcludedURL URL=$url"  >> $XCONF_LOG_FILE
-	   fi
+		else
+           echo "XCONF SCRIPT : Direct CDN enabled. Not setting CDL_SERVER_OVERRIDE for AutoExcludedURL URL $url"  >> $XCONF_LOG_FILE
         fi
     fi
 else
